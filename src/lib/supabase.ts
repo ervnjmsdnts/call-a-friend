3e59645c7eaf8d4d689b9391db9a2058236cfc9a
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-11-10 11:55:06 +0800

    Update supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,5 +4,11 @@ import {
 } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
-export const supabaseServer = createServerComponentClient({ cookies });
-export const supabaseRouter = createRouteHandlerClient({ cookies });
+const cookieStore = cookies();
+
+export const supabaseServer = createServerComponentClient({
+  cookies: () => cookieStore,
+});
+export const supabaseRouter = createRouteHandlerClient({
+  cookies: () => cookieStore,
+});
